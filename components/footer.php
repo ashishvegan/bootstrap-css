ad96b8c0d341cc75f2ea02718be3a242597455ab
Ashish Labade (Vegan) <54700101+ashishvegan@users.noreply.github.com>
2023-11-06 20:58:22 +0530

    Add files via upload

---
+++ components/footer.php
@@ -0,0 +1,7 @@
+<div class="container-flex">
+  <footer class="py-3 my-4">
+    <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+    </ul>
+    <p class="text-center text-muted">&copy; 2023 Channel 51, Dark Network</p>
+  </footer>
+</div>
---
+++ components/login-form.php
@@ -0,0 +1,23 @@
+<div class="container py-3">
+    <h3 class="display-4 border-bottom">Anonymous Login</h3>
+    <div class="row row-cols-1 row-cols-md-2">
+        <div class="col">
+            <div class="form-floating mb-3">
+                <input type="email" name="email" class="form-control" id="floatingInput" placeholder="name@example.com" required>
+                <label for="floatingInput">Email address</label>
+            </div>
+        </div>
+
+        <div class="col">
+            <div class="form-floating mb-3">
+                <input type="password" name="password" class="form-control" id="floatingPassword" placeholder="Password" required>
+                <label for="floatingPassword">Password</label>
+            </div>
+        </div>
+        <div class="col">
+            <input type="submit" class="btn btn-dark btn-lg" value="Login">
+        </div>
+        
+    </div>
+
+</div>
